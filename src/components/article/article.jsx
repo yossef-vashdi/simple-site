3e8a9755229766f4c0b2e4e4fe1aa1a9d0ee8c45
Yossef Vashdi <76695165+yossef-vashdi@users.noreply.github.com>
2021-11-06 13:32:33 +0200

    adjusting minimum requirements
    site ready to present in it's minimalistic form

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -3,13 +3,13 @@ import logo from "../../img/testImg.jpeg";
 
 function Article(props) {
   const { id, img, text, title } = props.content;
-  !id && console.log("no id !");
+  !id && img && console.log("no id !");
   return (
     <div className="article">
       <div className="articleTopBar"></div>
 
       <pre className="articleText">
-        <img src={logo} alt="im img" className="articleImg" />
+        {/* <img src={logo} alt="im img" className="articleImg" /> */}
         <pre className="articleTitle">{title}</pre>
         {text}
       </pre>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,34 +1,19 @@
 import Article from "../article/article";
 import "./main.css";
+import jsonData from "../../data.json";
 
-const list = [
-  {
-    title: "title one",
-    text: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-    img: "",
-    id: "1",
-  },
-  {
-    title: "title two",
-    text: "baba lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-    img: "",
-    id: "2",
-  },
-  {
-    title: "title three",
-    text: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
-    img: "",
-    id: "3",
-  },
-];
 function Main() {
   return (
     <div className="main">
-      <nav></nav>
-      {list.length !== 0 &&
-        list.map((item) => {
-          return <Article key={item.id} content={item} />;
-        })}
+      <nav>
+        <h1> Bits. </h1>
+      </nav>
+      <div className="container">
+        {jsonData.length !== 0 &&
+          jsonData.map((item) => {
+            return <Article key={item.id} content={item} />;
+          })}
+      </div>
     </div>
   );
 }
